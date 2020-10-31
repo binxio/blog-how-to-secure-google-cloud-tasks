2407d5251bb04aea0ab5e44e1fa6280df23172d1
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-10-31 12:17:43 +0100

    refactored to ./terraform

--- terraform/queuer.tf
+++ terraform/queuer.tf
@@ -4,6 +4,17 @@ resource "google_service_account" "queuer" {
   display_name = "app invoker queuer service account"
 }
 
+
+resource "google_service_account_key" "queuer" {
+  service_account_id = google_service_account.queuer.name
+}
+
+resource "local_file" "keys_queuer" {
+  content         = base64decode(google_service_account_key.queuer.private_key)
+  filename        = "keys/queuer.json"
+  file_permission = "0600"
+}
+
 # identities allowed to actAs the queuer
 resource "google_service_account_iam_binding" "queuer_service_account_users" {
   role               = "roles/iam.serviceAccountUser"
