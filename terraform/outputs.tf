cbba594c3a12174955433d931beb56a5af1b4d19
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-10-31 13:40:20 +0100

    make output so that it directly runs from the tf dir

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "commands" {
   value = <<EOF
 
 # call from python with service account key
-GOOGLE_APPLICATION_CREDENTIALS=./terraform/keys/queuer.json \
+GOOGLE_APPLICATION_CREDENTIALS=$PWD/keys/queuer.json \
   pipenv run python ../call.py \
     --queue ${google_cloud_tasks_queue.app.id} \
     --url ${google_cloud_run_service.app.status[0].url}/status \
