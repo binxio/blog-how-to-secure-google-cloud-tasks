d436eb9a28d12b084a48670d99aa611f6d427c23
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-10-31 13:47:04 +0100

    pretty output of the python run command

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,14 @@ output "commands" {
   value = <<EOF
 
 # call from python with service account key
-GOOGLE_APPLICATION_CREDENTIALS=$PWD/keys/queuer.json \
-  pipenv run python ../call.py \
-    --queue ${google_cloud_tasks_queue.app.id} \
-    --url ${google_cloud_run_service.app.status[0].url}/status \
-    --service-account ${google_service_account.invoker.email}
+(
+    cd ${dirname(abspath(path.module))}
+    GOOGLE_APPLICATION_CREDENTIALS=./terraform/keys/queuer.json \
+      pipenv run python ./call.py \
+	--queue ${google_cloud_tasks_queue.app.id} \
+	--url ${google_cloud_run_service.app.status[0].url}/status \
+	--service-account ${google_service_account.invoker.email}
+)
 
 # async call via gcloud
 gcloud tasks create-http-task \
