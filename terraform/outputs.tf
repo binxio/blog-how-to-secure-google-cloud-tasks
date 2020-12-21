3a8abc31f04da168078f6ed3604eb9c55c264777
Mark van Holsteijn <markvanholsteijn@binx.io>
2020-12-21 18:36:56 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,11 +14,11 @@ output "commands" {
 
 # async call via gcloud
 gcloud tasks create-http-task \
-	--queue ${google_cloud_tasks_queue.app.id} \
-	--url ${google_cloud_run_service.app.status[0].url}/status \
+    --url ${google_cloud_run_service.app.status[0].url}/status \
     --method GET \
-	--oidc--email ${google_service_account.invoker.email} \
-	--oidc-token-audience  ${google_cloud_run_service.app.status[0].url} \
+    --queue ${google_cloud_tasks_queue.app.id} \
+    --oidc--email ${google_service_account.invoker.email} \
+    --oidc-token-audience  ${google_cloud_run_service.app.status[0].url} \
     --impersonate-service-account ${google_service_account.queuer.email}
 
 EOF
