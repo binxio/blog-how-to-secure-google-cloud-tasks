d45f313569866e97dc08169922c968d672d591f5
Mark van Holsteijn <markvanholsteijn@binx.io>
2020-12-21 18:35:56 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,7 +8,8 @@ output "commands" {
       pipenv run python ./call.py \
 	--queue ${google_cloud_tasks_queue.app.id} \
 	--url ${google_cloud_run_service.app.status[0].url}/status \
-	--service-account ${google_service_account.invoker.email}
+	--service-account ${google_service_account.invoker.email} \
+        --impersonate-service-account ${google_service_account.queuer.email}
 )
 
 # async call via gcloud
